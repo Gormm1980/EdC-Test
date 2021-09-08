0c972ff04e1e62d10ba1ada8801c08904bc3c0ed
Gormm1980 <asgardtravel@gmail.com>
2021-09-08 22:07:27 +0200

    New react app

--- TodoApp/resources/js/config/constants.js
+++
@@ -1,9 +0,0 @@
-const constants = {
-  HOST_URL: 'https://laravel-react-boilerplate.herokuapp.com/api',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-};
-
-export default constants;
